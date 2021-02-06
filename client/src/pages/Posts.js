e54e5cd373f31236f3648bfe3474bc08c728744e
Anna Chirapongse <achirapongse@gmail.com>
2021-02-06 13:59:08 -0500

    The updates for the post

--- client/src/pages/Posts.js
+++ client/src/pages/Posts.js
@@ -7,9 +7,11 @@ import API from "../utils/API"
 class Posts extends Component {
   state = {
     title: "",
+    username: "",
+    throughts: "",
   };
 
-  // When the component mounts, get a list of all available base breeds and update this.state.breeds
+  // When the component mounts, get a list of all available past posts
   componentDidMount() {
     // API.getPostsList()
     //   .then(res => this.setState({ posts: res.data.message }))
@@ -35,12 +37,15 @@ class Posts extends Component {
     return (
       <div>
         <Container style={{ minHeight: "80%" }}>
-          <h1 className="text-center">Post</h1>
+          <h1 className="text-center">Posts</h1>
           <form>
             <input value={this.state.title} name="title" placeholder="Title" onChange={this.handleInputChange}/>
             <input value={this.state.username} name="username" placeholder="Username" onChange={this.handleInputChange}/> 
             <input value={this.state.thoughts} name="thoughts" placeholder="What is on your mind?" onChange={this.handleInputChange}/> 
           </form>
+          <button on Click={this.handleForSubmit} className="btn btn-success" type="submit">
+            Post
+          </button>
           {/* <Alert
             type="danger"
             style={{ opacity: this.state.error ? 1 : 0, marginBottom: 10 }}
