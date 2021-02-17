abaee54a6df22d866f270c8e1c62f57b3afcd282
Anna Chirapongse <achirapongse@gmail.com>
2021-02-16 21:29:19 -0500

    Updates to the styling

--- models/user.js
+++ models/user.js
@@ -3,15 +3,6 @@ const bcrypt = require("bcryptjs");
 // Creating our User model
 module.exports = function(sequelize, DataTypes) {
   const User = sequelize.define("User", {
-    // The email cannot be null, and must be a proper email before creation
-    // email: {
-    //   type: DataTypes.STRING,
-    //   allowNull: false,
-    //   unique: true,
-    //   validate: {
-    //     isEmail: true
-    //   }
-    // },
     username: {
       type: DataTypes.STRING,
       allowNull: false,
